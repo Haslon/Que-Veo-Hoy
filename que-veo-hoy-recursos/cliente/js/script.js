312509119adbb2d2395070a699cb2bdfa240e31b
Dantepor <xiancan333@gmail.com>
2024-06-28 15:47:33 -0500

    Update script.js

--- que-veo-hoy-recursos/cliente/js/script.js
+++ que-veo-hoy-recursos/cliente/js/script.js
@@ -1,48 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const preRegisteredUsers = [
-        { email: 'xiancan333@gmail.com', password: '89328893' },
-        { email: 'Elvis.ccs151@gmail.com', password: 'SiempreUnidos12' },
-        { email: '76234082@certus.edu.pe', password: 'FelizxSiempre903' },
-        { email: '74125288@certus.edu.pe', password: 'Soledad8932' },
-        { email: '77071313@certus.edu.pe', password: 'Antisistemas3141' },
-        { email: '72839383@certus.edu.pe', password: 'PowerRangers32' }
-    ];
-
-    preRegisteredUsers.forEach(user => {
-        localStorage.setItem(user.email, JSON.stringify(user));
-    });
-
     const loginForm = document.getElementById('loginForm');
-    const emailError = document.getElementById('emailError');
-    const passwordError = document.getElementById('passwordError');
 
-    loginForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        emailError.textContent = '';
-        passwordError.textContent = '';
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-        const email = document.getElementById('email').value.trim();
-        const password = document.getElementById('password').value.trim();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
 
-        if (email.length > 50 || password.length > 50) {
-            emailError.textContent = 'El email y la contraseña no pueden tener más de 50 caracteres';
-            return;
-        }
+        // Aquí puedes agregar la lógica de validación de login o de envío de datos al servidor
 
-        const storedUser = localStorage.getItem(email);
-        if (storedUser) {
-            const user = JSON.parse(storedUser);
-            if (user.password === password) {
-                alert('Login exitoso');
-                window.location.href = 'index.html';
-            } else {
-                passwordError.textContent = 'Contraseña incorrecta';
-            }
-        } else {
-            emailError.textContent = 'El usuario no existe';
-        }
+        // Si la validación es exitosa, redirige a index.html
+        window.location.href = 'index.html';
     });
 });
-
-
-
